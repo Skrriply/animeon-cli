ae5a9ee99ebc7e333ed1a088823241ad18f90fe7
Skrriply <176331248+Skrriply@users.noreply.github.com>
2025-01-01 14:19:52 +0200

    Extend anime metadata fields
    - Added new attributes to the Anime model
    - Added post-init methods to models
    - Renamed attribute 'id' to 'id_' to avoid shadowing built-in id() function

--- src/models/anime.py
+++ src/models/anime.py
@@ -1,4 +1,6 @@
+import re
 from dataclasses import dataclass
+from typing import List
 
 
 @dataclass
@@ -7,11 +9,59 @@ class Anime:
     Anime model.
 
     Attributes:
-        id: ID of the anime.
-        mal_id: MyAnimeList ID.
+        id_: ID of the anime.
         title: Title of the anime.
+        poster: URL of the anime poster.
+        rating: MyAnimeList rating of the anime.
+        scored_by: Number of users who scored the anime.
+        type_: Type of the anime. (TV, OVA, ONA, Movie or Special)
+        episodes: Number of episodes of the anime.
+        episodes_aired: Number of episodes aired of the anime.
+        status: Status of the anime.
+        genres: List of genres of the anime.
+        release_year: Year the anime was released.
+        studio: Studio that produced the anime.
+        producer: Producer of the anime.
+        description: Description of the anime.
+        mal_id: MyAnimeList ID of the anime.
     """
 
-    id: int
-    mal_id: int
+    id_: int
     title: str
+    poster: str
+    rating: float
+    scored_by: int
+    type_: str
+    episodes: int
+    episodes_aired: int
+    status: str
+    genres: List[str]
+    release_year: int
+    studio: str
+    producer: str
+    description: str
+    mal_id: int
+
+    def __post_init__(self) -> None:
+        """
+        Post initialization method.
+        """
+        if self.description:
+            self.description = self._clean_description(self.description)
+
+    @staticmethod
+    def _clean_description(text: str) -> str:
+        """
+        Cleans description from Markdown links.
+
+        Args:
+            text: Description to clean.
+
+        Returns:
+            Cleaned description.
+        """
+        # Removes Markdown links
+        text = re.sub(r"\[([^\]]+)\]\([^\)]+\)", r"\1", text)
+        text = re.sub(r"<(http[s]?://\S+)>", r"\1", text)
+
+        return text.strip()
